65e04df5239a4d01887d8032493d679303a432e3
become-iron <nikitaburkov@yandex.ru>
2015-10-11 13:08:33 +0300

    доб. функция автообновления, показываются формулы, доб. формулы

--- script.js
+++ script.js
@@ -4,37 +4,39 @@ var
     // выведены далеко не все формулы
 	inputs = [
         ['D', 'Номинальный размер', ['Dmax - ES']],
-        ['Dmax', 'Наибольший предельный размер отверстия', ['ES + D', 'TD + Dmin']],  // выв
-        ['Dmin', 'Наименьший предельный размер отверстия', ['EI + D', 'Dmax - TD']],  // выв
-        ['Dm', 'Средний размер отверстия', ['Em + D']],  // выв
-        ['dmax', 'Наибольший предельный размер вала', ['es + D', 'Td + dmin']],  // выв
-        ['dmin', 'Наименьший предельный размер вала', ['ei + D', 'dmax - Td']],  // выв
-        ['dm', 'Средний размер вала', ['em + D']],  // выв
+        ['Dmax', 'Наиб. пред. размер отверстия', ['ES + D', 'TD + Dmin']],  // выв
+        ['Dmin', 'Наим. пред. размер отверстия', ['EI + D', 'Dmax - TD']],  // выв
+        ['Dm', 'Средний размер отверстия', ['Em + D', '( Dmax + Dmin ) / 2']],  // выв
+        ['dmax', 'Наиб. пред. размер вала', ['es + D', 'Td + dmin']],  // выв
+        ['dmin', 'Наим. пред. размер вала', ['ei + D', 'dmax - Td']],  // выв
+        ['dm', 'Средний размер вала', ['em + D', '( dmax + dmin ) / 2']],  // выв
         ['ES', 'Верхнее отклонение отверстия', ['Dmax - D']],
         ['EI', 'Нижнее отклонение отверстия', ['Dmin - D']],
-        ['Em', 'Среднее отклонение отверстия', ['Dm - D']],
+        ['Em', 'Среднее отклонение отверстия', ['Dm - D', '( ES + EI ) / 2'/*выв*/]],
         ['es', 'Верхнее отклонение вала', ['dmax - D']],
         ['ei', 'Нижнее отклонение вала', ['dmin - D']],
-        ['em', 'Среднее отклонение вала', ['dm - D']],
-        ['Smax', 'Наибольший гарантированный зазор', ['Dmax - dmin', 'ES - ei']],
-        ['Smin', 'Наименьший гарантированный зазор', ['Dmin - dmax', 'EI - es']],
-        ['Sm', 'Средний (наиболее вероятный) зазор', ['Dm - dm', 'Em - em', '( Smax + Smin ) / 2']],
-        ['Nmax', 'Наибольший гарантированный натяг', ['dmax - Dmin',  'es - EI', '- Smin']],
-        ['Nmin', 'Наименьший гарантированный натяг', ['dmin - Dmax', 'ei - ES', '- Smax']],
-        ['Nm', 'Средний (наиболее вероятный) натяг', ['dm - DM', 'em - Em', '- Sm']],
-        ['TD', 'Допуск отверстия', ['Dmax - Dmin']],
-        ['Td', 'Допуск вала', ['dmax - dmin']],
-        ['Ts', 'Допуск зазора (посадки)', ['Smax - Smin', 'TD - Td']],
+        ['em', 'Среднее отклонение вала', ['dm - D', '( es + ei ) / 2'/*выв*/]],
+        ['Smax', 'Наиб. гарант. зазор', ['Dmax - dmin', 'ES - ei', '- Nmin'/*выв*/]],
+        ['Smin', 'Наим. гарант. зазор', ['Dmin - dmax', 'EI - es', '- Nmax'/*выв*/]],
+        ['Sm', 'Средний (наиб. вер.) зазор', ['Dm - dm', 'Em - em', '( Smax + Smin ) / 2', '- Nm'/*выв*/]],
+        ['Nmax', 'Наиб. гарант. натяг', ['dmax - Dmin',  'es - EI', '- Smin']],
+        ['Nmin', 'Наим. гарант. натяг', ['dmin - Dmax', 'ei - ES', '- Smax']],
+        ['Nm', 'Средний (наиб. вер.) натяг', ['dm - Dm', 'em - Em', '( Nmax + Nmin ) / 2', /*выв*/'- Sm']],
+        ['TD', 'Допуск отверстия', ['Dmax - Dmin', 'ES - EI'/*выв*/]],
+        ['Td', 'Допуск вала', ['dmax - dmin', 'es - ei'/*выв*/]],
+        ['Ts', 'Допуск зазора (посадки)', ['( Smax ) - ( Smin )', 'TD - Td']],
         ['TN', 'Допуск натяга (посадки)', ['( Nmax ) - ( Nmin )', 'TD - Td']]
     ];
 
 
 $(document).ready(function(){
     // генерация полей
+    //var mera;
     for (var i = 0; i < inputs.length; ++i){
-        $('table').append( $('<tr><td>' + inputs[i][0] + '</td><td><input value="" class="' + inputs[i][0] + '"></td><td>' + inputs[i][1] + '</td></tr>') );
+        //(inputs[i][0] ==    )
+        $('table').append( $('<tr><td>' + inputs[i][0] + '</td><td><input value="" class="' + inputs[i][0] + '"></td><td>' + inputs[i][1] + '</td><td>' +  inputs[i][2] +'</td></tr>') );
     }
-    $('table').append( $('<tr><td>Тип посадки:</td><td><div class="posadka">не определена</div></td><td></td></tr>') );
+    $('table').append( $('<tr><td>Тип посадки:</td><td><div class="fit">не определена</div></td><td></td></tr>') );
 
 
     function calculate(expressions){
@@ -47,15 +49,13 @@ $(document).ready(function(){
          */
         var sequence,
             fieldValue,  // значение считываемого поля
-            marker,  // достаточно ли данных для просчета выражения
-            sym;
+            marker;  // достаточно ли данных для просчета выражения
         for (var i = 0; i < expressions.length; i++) {
             marker = 0;
             sequence = expressions[i].split(' ');  // разбиваем выражение на составные части
             for (var j = 0; j < sequence.length; j++) {
                 // если символ, переходим на новую итерацию
-                sym = sequence[j];
-                if (sym == '=' || sym == '+' || sym == '-' || sym == '/' || sym == '(' || sym == ')' || sym == '' || sym == '2') {
+                if ('=+-/()2'.indexOf(sequence[j]) != -1) {  // костыль
                     continue;
                 }
                 else {
@@ -67,7 +67,7 @@ $(document).ready(function(){
                     }
                 }
             }
-            if (marker == 0) {break}
+            if (marker == 0) {break}  // если формула просчитана
         }
         if (marker == 1) {
             return '';
@@ -79,29 +79,38 @@ $(document).ready(function(){
         }
 	}
 
-
-    // просчёт значений
-    $("form").change(function() {
- 	//$(".calc").click(function(){
+    function calc(){
         var fieldValue;  // значение поля
- 		for (var i = 0; i < inputs.length; ++i) {
+        for (var i = 0; i < inputs.length; ++i) {
             fieldValue = '.' + inputs[i][0];
- 			if ($(fieldValue).val() == '') {
- 				$(fieldValue).val(calculate(inputs[i][2]));
- 			}
- 		}
+            if ($(fieldValue).val() == '') {
+                $(fieldValue).val(calculate(inputs[i][2]));
+            }
+        }
 
         // определение посадки
-        if ($('.Smin').val() == '0') {$('.posadka').text('скользящая посадка')}
-        if ($('.Nmin').val() == '0') {$('.posadka').text('легко прессовая посадка')}
-        if (($('.Smax').val() == $('.Nmax').val()) && $('.Smax').val()) {$('.posadka').text('переходная посадка')}
-	});
+        if ($('.Smin').val() == '0') {$('.fit').text('скользящая посадка')}
+        if ($('.Nmin').val() == '0') {$('.fit').text('легко прессовая посадка')}
+        if (($('.Smax').val() == $('.Nmax').val()) && $('.Smax').val()) {$('.fit').text('переходная посадка')}
 
+
+        // определение системы (отверстия или вала)
+
+    }
+
+
+    // просчёт значений
+    $('form').change(function() {
+        if ($('#check').prop("checked")) {  // если стоит флажок "Обновлять автоматически"
+            calc();
+        }
+    });
+    $(".calc").click(calc);
 
     // клик по кнопке сброса
     $('.reset').click(function(){
         $('.graph').css('visibility', 'hidden');
-        $('.posadka').text('не определена');
+        $('.fit').text('не определена');
     });
 
     $(".plot").click(function() {
