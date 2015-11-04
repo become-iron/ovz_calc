ff6f7c44a8b0d171fc3d48d868f30fae5b222aa7
become-iron <nikitaburkov@yandex.ru>
2015-11-04 16:45:38 +0300

    Добавлены квалитеты (7 и 8, для номинальных размеров 1-30, поля допусков g/G и h/H. Плюс ещё какие-то изменения

--- script.js
+++ script.js
@@ -1,9 +1,144 @@
 var
+    // квалитеты
+    qualitets = {
+        // ключ : квалитет
+        0 : '—',
+        //19  : '01',
+        //1  : '1',
+        //2  : '2',
+        //3  : '3',
+        //4  : '4',
+        //5  : '5',
+        //6  : '6',
+        7  : '7',
+        8  : '8'/*,*/
+        //9  : '9',
+        //10 : '10',
+        //11 : '11',
+        //12 : '12',
+        //13 : '13',
+        //14 : '14',
+        //15 : '15',
+        //16 : '16',
+        //17 : '17',
+        //18 : '18'
+    },
+
+    // поля допусков
+    tol_zones = {
+        // поле допуска : ключ
+        //0  : '—',
+        1  :  'a',
+        2  :  'b',
+        3  :  'c',
+        4  : 'cd',
+        5  :  'd',
+        6  :  'e',
+        7  : 'ef',
+        8  :  'f',
+        9  : 'fg',
+        10 :  'g',
+        11 :  'h',
+        12 : 'js',
+        13 :  'j',
+        14 :  'k',
+        15 :  'm',
+        16 :  'n',
+        17 :  'p',
+        18 :  'r',
+        19 :  's',
+        20 :  't',
+        21 :  'u',
+        22 :  'v',
+        23 :  'x',
+        24 :  'y',
+        25 :  'z',
+        26 : 'za',
+        27 : 'zb',
+        28 : 'zc',
+        29 :  'A',
+        30 :  'B',
+        31 :  'C',
+        32 : 'CD',
+        33 :  'D',
+        34 :  'E',
+        35 : 'EF',
+        36 :  'F',
+        37 : 'FG',
+        38 :  'G',
+        39 :  'H',
+        40 : 'JS',
+        41 :  'J',
+        42 :  'K',
+        43 :  'M',
+        44 :  'N',
+        45 :  'P',
+        46 :  'R',
+        47 :  'S',
+        48 :  'T',
+        49 :  'U',
+        50 :  'V',
+        51 :  'X',
+        52 :  'Y',
+        53 :  'Z',
+        54 : 'ZA',
+        55 : 'ZB',
+        56 : 'ZC'
+    },
+    // ОТВЕРСТИЯ
+    variationsHoles = [
+        // указываются коды
+        //[предел размеров, квалитет, поле допуска, [верхний предел, нижний предел]]
+        [0, 7, 38, [12, 2]],
+        [1, 7, 38, [16, 4]],
+        [2, 7, 38, [20, 5]],
+        [3, 7, 38, [24, 6]],
+        [4, 7, 38, [24, 6]],
+        [5, 7, 38, [28, 7]],
+        [6, 7, 38, [28, 7]],
+        [0, 7, 39, [10, 0]],
+        [1, 7, 39, [12, 0]],
+        [2, 7, 39, [15, 0]],
+        [3, 7, 39, [18, 0]],
+        [4, 7, 39, [18, 0]],
+        [5, 7, 39, [21, 0]],
+        [6, 7, 39, [21, 0]],
+        [0, 8, 38, [12, 2]],
+        [1, 8, 38, [16, 4]],
+        [2, 8, 38, [20, 5]],
+        [3, 8, 38, [24, 6]],
+        [4, 8, 38, [24, 6]],
+        [5, 8, 38, [28, 7]],
+        [6, 8, 38, [28, 7]],
+        [0, 8, 39, [10, 0]],
+        [1, 8, 39, [12, 0]],
+        [2, 8, 39, [15, 0]],
+        [3, 8, 39, [18, 0]],
+        [4, 8, 39, [18, 0]],
+        [5, 8, 39, [21, 0]],
+        [6, 8, 39, [21, 0]]],
+    // ВАЛЫ
+    variationsShifts = [
+        [0, 7, 11, [0, -10]],
+        [1, 7, 11, [0, -12]],
+        [2, 7, 11, [0, -15]],
+        [3, 7, 11, [0, -18]],
+        [4, 7, 11, [0, -18]],
+        [5, 7, 11, [0, -21]],
+        [6, 7, 11, [0, -21]],
+        [0, 8, 11, [0, -14]],
+        [1, 8, 11, [0, -18]],
+        [2, 8, 11, [0, -22]],
+        [3, 8, 11, [0, -27]],
+        [4, 8, 11, [0, -27]],
+        [5, 8, 11, [0, -33]],
+        [6, 8, 11, [0, -33]]],
+
     // величины, формулы их расчёта и описания
     // "выв" — выведенная формула
     // выведены далеко не все формулы
 	inputs = [
-        ['D', 'Номинальный размер', ['Dmax - ES']],
+        //['D', 'Номинальный размер', ['Dmax - ES']],
         ['Dmax', 'Наиб. пред. размер отверстия', ['ES + D', 'TD + Dmin']],  // выв
         ['Dmin', 'Наим. пред. размер отверстия', ['EI + D', 'Dmax - TD']],  // выв
         ['Dm', 'Средний размер отверстия', ['Em + D', '( Dmax + Dmin ) / 2']],  // выв
@@ -27,18 +162,42 @@ var
         ['Ts', 'Допуск зазора (посадки)', ['Smax - Smin', 'TD - Td']],
         ['TN', 'Допуск натяга (посадки)', ['Nmax - Nmin', 'TD - Td']]
     ],
+
     add_fields = [['fit', 'Посадка:', 'неизвестна'], ['sys_fit', 'Cистема:', 'неизвестна']],
     reFieldValue = /^(\-?[0-9]+(\.?[0-9]+)?)?$/,  // валидность числа TODO улучшить
-    fieldName;
+    fieldName,
+    fields = [
+        ['qual qual_hole', 'Квалитет отверстия'],
+        ['tol_zone zone_hole', 'Поле допуска отверстия'],
+        ['qual qual_shift', 'Квалитет вала'],
+        ['tol_zone zone_shift', 'Поле допуска вала']
+    ],
+    code = '';
+
+var nom_zone;
 
 
 $(document).ready(function(){
-    // генерация полей
+
+    // ГЕНЕРАЦИЯ ПОЛЕЙ
+    code += '<tr><td>D</td><td><input type="number" min="1" max="500" step="1" class="field D"></td><td>Номинальный размер</td><td></td></tr>';
+    for (var k = 0; k < fields.length; k++) {
+        code += '<tr><td></td><td><select class="' + fields[k][0] + '"></select></td><td>' + fields[k][1] + '</td><td></td></tr>'
+    }
     for (var i = 0; i < inputs.length; ++i){
-        $('table').append( $('<tr><td>' + inputs[i][0] + '</td><td><input type="text" class="field ' + inputs[i][0] + '"></td><td>' + inputs[i][1] + '</td><td>' +  inputs[i][2] +'</td></tr>') );
+        code += '<tr><td>' + inputs[i][0] + '</td><td><input type="text" class="field ' + inputs[i][0] + '"></td><td>' + inputs[i][1] + '</td><td>' +  inputs[i][2] +'</td></tr>';
     }
     for (var j = 0; j < add_fields.length; j++) {
-        $('table').append( $('<tr><td>' + add_fields[j][1] + '</td><td><div class="' + add_fields[j][0] + '">' + add_fields[j][2] + '</div></td><td></td></tr>') );
+        code += '<tr><td>' + add_fields[j][1] + '</td><td><div class="' + add_fields[j][0] + '">' + add_fields[j][2] + '</div></td><td></td></tr>';
+    }
+    $('table').append(code);
+    $(".zone_hole").prop("disabled", true);
+    $(".zone_shift").prop("disabled", true);
+    for (var key in qualitets) {  // TODO
+        if (qualitets.hasOwnProperty(key) && /^0$|^[1-9]\d*$/.test(key) && key <= 4294967294) {
+            $('.qual_hole').append($('<option value="' + key + '">' + qualitets[key] + '</option>'));
+            $('.qual_shift').append($('<option value="' + key + '">' + qualitets[key] + '</option>'));
+        }
     }
 
 
@@ -57,7 +216,7 @@ $(document).ready(function(){
             marker = 0;
             sequence = expression[i].split(' ');  // разбиваем выражение на составные части
             for (var j = 0; j < sequence.length; j++) {
-                if (['=', '+', '-', '/', '(', ')', '2'].indexOf(sequence[j]) == -1) {  // если не символ
+                if (['=', '+', '-', '/', '(', ')', '2'].indexOf(sequence[j]) === -1) {  // если не символ
                     fieldValue = $('.' + sequence[j]).val();  // получаем значение поля
                     if (fieldValue){sequence[j] = '(' + fieldValue + '*10000)'}  // записываем значение в последовательность
                     else {  // если значение отсутствует, выходим из цикла
@@ -66,9 +225,9 @@ $(document).ready(function(){
                     }
                 }
             }
-            if (marker == 0) {break}  // если формула просчитана
+            if (marker === 0) {break}  // если формула просчитана
         }
-        if (marker == 1) {
+        if (marker === 1) {
             return '';
         }
         else {
@@ -78,27 +237,121 @@ $(document).ready(function(){
         }
 	}
 
-    function calc(){
+    function calc() {
         for (var i = 0; i < inputs.length; ++i) {
             fieldName = '.' + inputs[i][0];
             //$(fieldName).val( $(fieldName).val().replace(',', '.') );
-            if ($(fieldName).val() == '') {  // если поле пустое, пробуем для него просчитать значение
+            if ($(fieldName).val() === '') {  // если поле пустое, пробуем для него просчитать значение
                 $(fieldName).val(calculate(inputs[i][2]));
             }
         }
 
         // определение посадки
-        if ($('.Smin').val() == '0') {$('.fit').text('скользящая')}
-        else if ($('.Nmin').val() == '0') {$('.fit').text('легко прессовая')}
-        else if (($('.Smax').val() == $('.Nmax').val()) && $('.Smax').val()) {$('.fit').text('переходная')}
+        if ($('.Smin').val() === '0') {$('.fit').text('скользящая')}
+        else if ($('.Nmin').val() === '0') {$('.fit').text('легко прессовая')}
+        else if (($('.Smax').val() === $('.Nmax').val()) && $('.Smax').val()) {$('.fit').text('переходная')}
         else {$('.fit').text('неизвестна')}
 
         // определение системы (отверстия или вала)
-        if ($('.EI').val() == '0') {$('.sys_fit').text('отверстия')}
-        else if ($('.es').val() == '0') {$('.sys_fit').text('вала')}
+        if ($('.EI').val() === '0') {$('.sys_fit').text('отверстия')}
+        else if ($('.es').val() === '0') {$('.sys_fit').text('вала')}
         else {$('.sys_fit').text('неизвестна')}
     }
 
+    function select_tol_zones() {
+        // определение области номинального размера
+        var tmpValField = Number($('.D').val());
+        if (1 <= tmpValField && tmpValField <= 500) {
+            if (1 <= tmpValField && tmpValField <= 3) {
+                nom_zone = 0;
+            }
+            else if (3 < tmpValField && tmpValField <= 6) {
+                nom_zone = 1;
+            }
+            else if (6 < tmpValField && tmpValField <= 10) {
+                nom_zone = 2;
+            }
+            else if (10 < tmpValField && tmpValField <= 14) {
+                nom_zone = 3;
+            }
+            else if (14 < tmpValField && tmpValField <= 18) {
+                nom_zone = 4;
+            }
+            else if (18 < tmpValField && tmpValField <= 24) {
+                nom_zone = 5;
+            }
+            else if (24 < tmpValField && tmpValField <= 30) {
+                nom_zone = 6;
+            }
+            //$(".qual_hole").prop("disabled", false);
+            //$(".qual_shift").prop("disabled", false);
+        }
+        else {
+            $(".zone_hole").prop("disabled", true);
+            $(".zone_shift").prop("disabled", true);
+        }
+
+        var qual_shift = $('.qual_shift').val(),
+            qual_hole = $('.qual_hole').val(),
+            lstHoleZones = [],
+            lstShiftZones = [];
+        for (i = 0; i < variationsHoles.length; i++) {
+            if (variationsHoles[i][0] == nom_zone && variationsHoles[i][1] == qual_hole && lstHoleZones.indexOf(variationsHoles[i][2]) == -1) {
+                lstHoleZones.push(variationsHoles[i][2]);
+            }
+        }
+        for (var i = 0; i < variationsShifts.length; i++) {
+            if (variationsShifts[i][0] == nom_zone && variationsShifts[i][1] == qual_shift && lstShiftZones.indexOf(variationsShifts[i][2]) == -1) {
+                lstShiftZones.push(variationsShifts[i][2]);
+            }
+        }
+
+        $('.zone_hole').empty();
+        $('.zone_shift').empty();
+        if (lstHoleZones.length > 0) {
+            $('.zone_hole').append($('<option value="100">—</option>'));
+            for (i = 0; i < lstHoleZones.length; i++) {
+                $('.zone_hole').append($('<option value="' + lstHoleZones[i] + '">' + tol_zones[lstHoleZones[i]] + '</option>'));
+                $(".zone_hole").prop("disabled", false);
+            }
+        }
+        else {$(".zone_hole").prop("disabled", true);}
+        if (lstShiftZones.length > 0) {
+            $('.zone_shift').append($('<option value="100">—</option>'));
+            for (i = 0; i < lstShiftZones.length; i++) {
+                $('.zone_shift').append($('<option value="' + lstShiftZones[i] + '">' + tol_zones[lstShiftZones[i]] + '</option>'));
+                $(".zone_shift").prop("disabled", false);
+            }
+        }
+        else {$(".zone_shift").prop("disabled", true);}
+    }
+
+
+
+    // выбор квалитета
+    $('.qual').change( function() {
+        select_tol_zones();
+    });
+    //$('.D').change( function() {
+    //    select_tol_zones();
+    //});
+
+
+    // выбор поля допуска
+    $('.tol_zone').change( function() {
+        var valQualHole = Number( $('.qual_hole :selected').val() ),
+            valQualShift = Number( $('.qual_shift :selected').val() );
+        if (valQualHole > 0) {
+            // TODO если не будет искомого элемента, может вернуться что-то странное, наверное
+            $('.ES').val(variationsHoles[valQualHole][3][0] / 1000);
+            $('.EI').val(variationsHoles[valQualHole][3][1] / 1000);
+        }
+        if (valQualShift > 0) {
+            // TODO если не будет искомого элемента, может вернуться что-то странное, наверное
+            $('.es').val(variationsShifts[valQualShift][3][0] / 1000);
+            $('.ei').val(variationsShifts[valQualShift][3][1] / 1000);
+        }
+    });
 
     // просчёт значений
     $('form').change( function() {
@@ -109,10 +362,11 @@ $(document).ready(function(){
     $(".calc").click(calc); // по клику по кнопке 'Считать'
 
     // проверка валидности значений в полях
-    $('form').keyup( function() {  // TODO улучшить
+    $('input').keyup( function() {  // TODO улучшить, не работает для поля "D"
         for (var i = 0; i < inputs.length; ++i) {
             fieldName = '.' + inputs[i][0];
             $(fieldName).val( $(fieldName).val().replace(',', '.') );
+            // TODO добавить проверку значений для пределов
             if (reFieldValue.test($(fieldName).val())) {
                 $(fieldName).css('background-color', 'white');
             }
@@ -127,10 +381,12 @@ $(document).ready(function(){
         $('.graph').css('visibility', 'hidden');
         $('.fit').text('неизвестна');
         $('.sys_fit').text('неизвестна');
+        $(".zone_hole").prop("disabled", true);
+        $(".zone_shift").prop("disabled", true);
     });
 
     $(".plot").click(function() {
-        if ($('.graph').css('visibility') == 'hidden') {
+        if ($('.graph').css('visibility') === 'hidden') {
             $('.graph').css('visibility', 'visible');
         }
         else {
