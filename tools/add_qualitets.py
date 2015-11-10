de075a63470b01a4af35d3ff7026fb633633d79b
Alex <sasha-ushakov@mail.ru>
2015-11-10 23:37:18 +0300

    Добавлены поля допусков h6 - P6

--- tools/add_qualitets.py
+++ tools/add_qualitets.py
@@ -25,25 +25,49 @@ for i in range(25):
         j = 1
         continue
 
-    if n in [12, 40]:
-        x = float(input('Верхний предел '))
-        y = -x
-    else:
-        if n == 11:
-            x = 0
+    try:
+        if n in [12, 40]:
+            x = input('Верхний предел ')
+            try:
+                x = int(x)
+            except ValueError:
+                x = float(x)
+            y = -x
         else:
-            x = int(input('Верхний предел '))
+            if n == 11:
+                x = 0
+            else:
+                x = int(input('Верхний предел '))
 
-        if n == 39:
-            y = 0
-        else:
-            y = int(input('Нижний предел '))
+            if n == 39:
+                y = 0
+            else:
+                y = int(input('Нижний предел '))
+    except ValueError:
+        x = ""
+        y = ""
 
     if n <= 11 or n >= 44:
         x = -x
         y = -y
     a.append([p, i, k, n, [x, y]])
-    if n < 18 or 28 < n < 46:
+
+    if n in [18, 19, 46, 47]:
+        if i in [3, 5, 7]:
+            a.append([p, i+1, k, n, [x, y]])
+            j = 1
+
+    elif n in [1, 2, 3, 31]:
+        if i in [3, 5]:
+            a.append([p, i+1, k, n, [x, y]])
+            j = 1
+
+    elif n in [20, 21, 48, 49]:
+        if i in [3]:
+            a.append([p, i+1, k, n, [x, y]])
+            j = 1
+
+    elif n in list(list(range(0, 18)) + list(range(29, 46))):
         if i in [3, 5, 7, 9, 11, 19, 21, 23]:
             a.append([p, i+1, k, n, [x, y]])
             j = 1
@@ -51,10 +75,7 @@ for i in range(25):
             a.append([p, i+1, k, n, [x, y]])
             a.append([p, i+2, k, n, [x, y]])
             j = 2
-    else:
-        if i in [3, 5, 7]:
-            a.append([p, i+1, k, n, [x, y]])
-            j = 1
+
 
 file_path = 'result'
 with open(file_path, "w", encoding="utf-8") as w:
